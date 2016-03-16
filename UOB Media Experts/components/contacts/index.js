2b634c6386d87f14e3f8520008b86074a0744cda
Richard Clifford <st00ka@yahoo.com>
2016-03-16 16:00:19 +0000

    Displaying Contact List fro JSON Feed

---
+++ UOB Media Experts/components/contacts/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+var dataStaff = new kendo.data.DataSource({
+    transport: {
+        read: {
+            url: "http://www.birmingham.ac.uk/web_services/Staff.svc/",
+            dataType: "json"
+        }
+    },
+    schema: {
+        parse: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                data[i].letter = data[i].LastName.trim().toUpperCase().charAt(0);
+            }
+            return data;
+        }
+    },
+    group: {field: "letter"}
+});
+
+
+app.expertList = kendo.observable({
+    title: "Media Experts",
+    onShow: function () {
+
+
+    },
+    dataSource: dataStaff,
+
+        error: function (e) {
+            console.log(e);
+        }
+
+});
